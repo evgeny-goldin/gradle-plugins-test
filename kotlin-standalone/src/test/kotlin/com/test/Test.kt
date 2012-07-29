23382496a8cfe0b292ee557e4dbb56acad43cbca
Evgeny Goldin <evgenyg@gmail.com>
2012-07-29 17:41:38 +0200

    "kotlin-standalone" module added.

---
+++ kotlin-standalone/src/test/kotlin/com/test/Test.kt
@@ -0,0 +1,34 @@
+package com.test
+
+import org.junit.Test
+
+
+class TestKotlin2
+{
+    [Test]
+    fun testBottles()
+    {
+        bottles.printBottles( 5 )
+    }
+
+
+    [Test]
+    fun testHtmlBuilder()
+    {
+        html.main( array( "a", "b" ))
+    }
+
+
+    [Test]
+    fun testLife()
+    {
+        life.main( array())
+    }
+
+
+    [Test]
+    fun testMaze()
+    {
+        maze.main( array())
+    }
+}
