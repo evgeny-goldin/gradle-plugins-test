edbdd00b6bfdb8eb6035b0eaf94a7402d561d980
Evgeny Goldin <evgenyg@gmail.com>
2012-09-29 23:00:26 +0200

    Java test update.

--- kotlin/java-depends-on-kotlin/src/test/java/com/test/TestJava.java
+++ kotlin/java-depends-on-kotlin/src/test/java/com/test/TestJava.java
@@ -1,7 +1,6 @@
 package com.test;
 
 import static junit.framework.Assert.assertEquals;
-import static junit.framework.TestCase.assertTrue;
 import org.junit.Test;
 
 
@@ -14,10 +13,7 @@ public class TestJava
     @Test
     public void testYouTrackRestJava ()
     {
-        assertTrue  ( new YouTrackRestJava( "http://rest-clients.myjetbrains.com/youtrack/" ).
-                      issueExists( "pl-359" ));
-        assertEquals( "Fixed",
-                      new YouTrackRestJava( "http://evgeny-goldin.org/youtrack/" ).
-                      issue( "pl-645" ).getCustomField( "State" ) );
+        assertEquals( "pl-359", new YouTrackRestJava( "http://rest-clients.myjetbrains.com/youtrack/" ).issue( "pl-359" ).getId());
+        assertEquals( "Fixed",  new YouTrackRestJava( "http://evgeny-goldin.org/youtrack/" ).issue( "pl-645" ).getCustomField( "State" ));
     }
 }
