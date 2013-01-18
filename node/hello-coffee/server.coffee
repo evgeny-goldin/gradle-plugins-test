09528cf2d2d8990a5b8a1bacde3cd344ab7a69aa
Evgeny Goldin <evgenyg@gmail.com>
2013-01-18 02:40:54 +0100

    Node plugin tests update - ports are configured dynamically.

--- node/hello-coffee/server.coffee
+++ node/hello-coffee/server.coffee
@@ -1,8 +1,11 @@
-http = require( 'http' )
+fs     = require( 'fs' )
+http   = require( 'http' )
+config = JSON.parse( fs.readFileSync( 'config/development.json', 'ascii' ))
+port   = config.port
 
 http.createServer(( req, res ) ->
   res.writeHead 200, 'Content-Type': 'text/plain'
   res.end 'Hello World - coffee\n'
-).listen 1337, '127.0.0.1'
+).listen port, '127.0.0.1'
 
-console.log 'Server running at http://127.0.0.1:1337/'
+console.log 'Server running at http://127.0.0.1:' + port + '/'
--- node/hello-js/server.js
+++ node/hello-js/server.js
@@ -1,8 +1,12 @@
-var http = require( 'http' )
+
+var fs     = require( 'fs' )
+var http   = require( 'http' )
+var config = JSON.parse( fs.readFileSync( 'config/development.json', 'ascii' ))
+var port   = config.port
 
 http.createServer( function ( req, res ) {
   res.writeHead( 200, { 'Content-Type': 'text/plain' })
   res.end( 'Hello World - js\n' )
-}).listen( 1337, '127.0.0.1' )
+}).listen( port, '127.0.0.1' )
 
-console.log( 'Server running at http://127.0.0.1:1337/' )
+console.log( 'Server running at http://127.0.0.1:' + port + '/' )
